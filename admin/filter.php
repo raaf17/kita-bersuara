4ee3c7f8db6d5e208f778c13ffe51595050c0d6a
yumaaji <yumaaji126@gmail.com>
2023-10-30 11:56:04 +0700

    mengubah beberapa tulisan di dashboard admin

--- admin/filter.php
+++ admin/filter.php
@@ -37,7 +37,7 @@ if ($_SESSION['status_login'] != true) {
       <p>Dashboard Admin</p>
       <div class="profile_siswa">
         <div class="name">
-          <p><span>Selamat Datang(Admin), </span>
+          <p><span>Selamat Datang, </span>
             <?php echo $_SESSION['nisn']['nama']; ?>
           </p>
           <img src="../assets/img/arrow-drop.png" alt="Arrow Drop" />
