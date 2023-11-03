26e47f0cd5e2c9a1e7f35acff982f4174dcf3922
raaf17 <raafimuhammadd123@gmail.com>
2023-11-03 11:03:23 +0700

    memperbaiki halaman data sisw

--- siswa/dashboardsiswa.php
+++ siswa/dashboardsiswa.php
@@ -12,7 +12,7 @@ if ($_SESSION['nisn'] != true) {
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Dashboard Siswa</title>
+  <title>Dashboard Siswa | KitaBersuara</title>
   <link rel="stylesheet" href="../style/profile.css?version=<?php echo filemtime('../style/profile.css'); ?>">
 </head>
 
--- siswa/pengaduan.php
+++ siswa/pengaduan.php
@@ -14,7 +14,7 @@ include '../admin/conn.php';
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Pengaduan | Siswa</title>
+  <title>Pengaduan | KitaBersuara</title>
   <link rel="stylesheet" href="../style/pengaduan.css?version=<?php echo filemtime('../style/pengaduan.css'); ?>">
 </head>
 
