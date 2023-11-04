e8e9c7b87c5d4f6cad7e175838ad3f36d345f2a6
raaf17 <raafimuhammadd123@gmail.com>
2023-11-04 09:40:34 +0700

    memperbaiki halaman filter

--- process/all_filter.php
+++ process/all_filter.php
@@ -1,5 +1,9 @@
 <?php
 
+if (!isset($_POST['filter-tgl'])) { ?>
+      <h1 style="text-align: center; font-weight: bold; font-size: 16px; margin-top: 2px; margin-bottom: -30px">Data masih kosong, <br>Pilih tanggal untuk menemukan data yang anda inginkan</h1>
+<?php }
+
 if (isset($_POST['filter-tgl'])) {
 
   $tgl_mulai = $_POST['tgl-mulai'];
@@ -9,62 +13,62 @@ if (isset($_POST['filter-tgl'])) {
   $unapprove = $conn->query("SELECT * FROM laporan,siswa,kategori,status_laporan where status_laporan.status='unapprove' and status_laporan.id_status=laporan.id_status and laporan.nisn=siswa.nisn and laporan.id_kategori=kategori.id_kategori AND laporan.created_at BETWEEN '$tgl_mulai' AND DATE_ADD('$tgl_selesai', INTERVAL 1 DAY)");
 
   if (mysqli_num_rows($approve) == 0 && mysqli_num_rows($unapprove) == 0) { ?>
-    <h1 style="text-align: center; font-weight: bold; font-size: 16px; margin-top: 5px; margin-bottom: -188px">Laporan yang anda cari pada<br>tanggal <?php echo $tgl_mulai ?> sampai dengan tanggal <?php echo $tgl_selesai ?> tidak ada!</h1>
-  <?php
+    <h1 style="text-align: center; font-weight: bold; font-size: 16px; margin-top: 5px; margin-bottom: -20px">Laporan yang anda cari pada<br>tanggal <?php echo $tgl_mulai ?> sampai dengan tanggal <?php echo $tgl_selesai ?> tidak ada!</h1>
+    <?php
   } else {
 
-  while ($perlaporan1 = $approve->fetch_assoc()) {; ?>
-  <div>
-    <div class="laporan">
-      <img src="../assets/fotobukti/<?= $perlaporan1['foto']; ?>" alt="bukti_laporan" width="170" height="170" />
-      <div class="detail_laporan">
-        <h4 class="pengusul">Pengusul: <span><?php echo $perlaporan1['nama']; ?></span></h3>
-          <h4 class="category">&nbsp;&nbsp;&nbsp;&nbsp;#<span><?php echo $perlaporan1['nama_kategori']; ?></span></h4>
-          <p>
-            Usulan: <br />
-            <span><?php echo $perlaporan1['keluhan'] ?>.</span>
-          </p>
-      </div>
-      <form action="" method="" class="status">
-        <div class="icon_status approved">
-          <img src="../assets/img/unapproved.png" alt="status-icon">
-          <p>APPROVE</p>
+    while ($perlaporan1 = $approve->fetch_assoc()) {; ?>
+      <div>
+        <div class="laporan">
+          <img src="../assets/fotobukti/<?= $perlaporan1['foto']; ?>" alt="bukti_laporan" width="170" height="170" />
+          <div class="detail_laporan">
+            <h4 class="pengusul">Pengusul: <span><?php echo $perlaporan1['nama']; ?></span></h3>
+              <h4 class="category">&nbsp;&nbsp;&nbsp;&nbsp;#<span><?php echo $perlaporan1['nama_kategori']; ?></span></h4>
+              <p>
+                Usulan: <br />
+                <span><?php echo $perlaporan1['keluhan'] ?>.</span>
+              </p>
+          </div>
+          <form action="" method="" class="status">
+            <div class="icon_status approved">
+              <img src="../assets/img/unapproved.png" alt="status-icon">
+              <p>APPROVE</p>
+            </div>
         </div>
-    </div>
-    <div class="form">
-      <textarea placeholder="tidak ada feedback" <?php if ($perlaporan1['feedback']) {
-                                                    echo "readonly";
-                                                  } ?> name="feedback" id="feedback" cols="10" rows="1" required><?php echo $perlaporan1['feedback']; ?></textarea>
-    </div>
-    </form>
-  </div>
-<?php } ?>
+        <div class="form">
+          <textarea placeholder="tidak ada feedback" <?php if ($perlaporan1['feedback']) {
+                                                        echo "readonly";
+                                                      } ?> name="feedback" id="feedback" cols="10" rows="1" required><?php echo $perlaporan1['feedback']; ?></textarea>
+        </div>
+        </form>
+      </div>
+    <?php } ?>
 
-<?php while ($perlaporan2 = $unapprove->fetch_assoc()) {; ?>
-  <div>
-    <div class="laporan">
-      <img src="../assets/fotobukti/<?= $perlaporan2['foto']; ?>" alt="bukti_laporan" width="170" height="170" />
-      <div class="detail_laporan">
-        <h4 class="pengusul">Pengusul: <span><?php echo $perlaporan2['nama']; ?></span></h3>
-          <h4 class="category">&nbsp;&nbsp;&nbsp;&nbsp;#<span><?php echo $perlaporan2['nama_kategori']; ?></span></h4>
-          <p>
-            Usulan: <br />
-            <span><?php echo $perlaporan2['keluhan'] ?>.</span>
-          </p>
-      </div>
-      <form action="" method="" class="status">
-        <div class="icon_status unapproved">
-          <img src="../assets/img/unapproved.png" alt="status-icon">
-          <p>UNAPPROVE</p>
+    <?php while ($perlaporan2 = $unapprove->fetch_assoc()) {; ?>
+      <div>
+        <div class="laporan">
+          <img src="../assets/fotobukti/<?= $perlaporan2['foto']; ?>" alt="bukti_laporan" width="170" height="170" />
+          <div class="detail_laporan">
+            <h4 class="pengusul">Pengusul: <span><?php echo $perlaporan2['nama']; ?></span></h3>
+              <h4 class="category">&nbsp;&nbsp;&nbsp;&nbsp;#<span><?php echo $perlaporan2['nama_kategori']; ?></span></h4>
+              <p>
+                Usulan: <br />
+                <span><?php echo $perlaporan2['keluhan'] ?>.</span>
+              </p>
+          </div>
+          <form action="" method="" class="status">
+            <div class="icon_status unapproved">
+              <img src="../assets/img/unapproved.png" alt="status-icon">
+              <p>UNAPPROVE</p>
+            </div>
         </div>
-    </div>
-    <div class="form">
-      <textarea placeholder="tidak ada feedback" <?php if ($perlaporan2['feedback']) {
-                                                    echo "readonly";
-                                                  } ?> name="feedback" id="feedback" cols="10" rows="1" required><?php echo $perlaporan2['feedback']; ?></textarea>
-    </div>
-    </form>
-  </div>
+        <div class="form">
+          <textarea placeholder="tidak ada feedback" <?php if ($perlaporan2['feedback']) {
+                                                        echo "readonly";
+                                                      } ?> name="feedback" id="feedback" cols="10" rows="1" required><?php echo $perlaporan2['feedback']; ?></textarea>
+        </div>
+        </form>
+      </div>
 <?php }
   }
 }
--- process/category.php
+++ process/category.php
@@ -7,21 +7,21 @@ $approve  = $conn->query("SELECT * FROM laporan,siswa,kategori,status_laporan wh
 $unapprove = $conn->query("SELECT * FROM laporan,siswa,kategori,status_laporan where kategori.nama_kategori='$keyword' and status_laporan.status='unapprove' and status_laporan.id_status=laporan.id_status and laporan.nisn=siswa.nisn and laporan.id_kategori=kategori.id_kategori ");
 
 if ($keyword == "Kesiswaan" or "Kurikulum" or "Sarpras" or "Humas") { ?>
-<!-- laporan dengan status terkirim -->
+    <!-- laporan dengan status terkirim -->
     <?php while ($perlaporan = $terkirim->fetch_assoc()) {; ?>
         <div>
             <div class="laporan">
                 <img src="../assets/fotobukti/<?= $perlaporan['foto']; ?>" alt="bukti_laporan" width="170" height="170" />
                 <div class="detail_laporan">
                     <h4 class="pengusul">Pengusul: <span><?php echo $perlaporan['nama']; ?></span></h3>
-                            <h4 class="category">&nbsp;&nbsp;&nbsp;&nbsp;#<span><?php echo $perlaporan['nama_kategori']; ?></span></h4>
-                            <p>
-                                Usulan: <br />
-                                <span><?php echo $perlaporan['keluhan'] ?>.</span>
-                            </p>
+                        <h4 class="category">&nbsp;&nbsp;&nbsp;&nbsp;#<span><?php echo $perlaporan['nama_kategori']; ?></span></h4>
+                        <p>
+                            Usulan: <br />
+                            <span><?php echo $perlaporan['keluhan'] ?>.</span>
+                        </p>
                 </div>
                 <form action="" method="post" class="status">
-                    <input type="text" name="id_status" value="<?php echo $perlaporan['id_status'];  ?>"  hidden>
+                    <input type="text" name="id_status" value="<?php echo $perlaporan['id_status'];  ?>" hidden>
                     <button type="submit" name="approve" class="approve">APPROVE</button>
                     <button type="submit" name="unapprove" class="delete">DELETE</button>
             </div>
@@ -32,73 +32,77 @@ if ($keyword == "Kesiswaan" or "Kurikulum" or "Sarpras" or "Humas") { ?>
         </div>
     <?php } ?>
     <?php
-            if (isset($_POST['approve'])) {
-                $id_status = $_POST['id_status'];
-                $feedback = $_POST['feedback'];
-                $conn->query("UPDATE status_laporan SET feedback='$feedback', status='approve' where id_status='$id_status'");
-                echo "<script>alert('Laporan Berhasil Di Approve')</script>";
-                echo "<script>location='dashboard.php';</script>";
-            }
-            ?>
-            <?php
-            if (isset($_POST['unapprove'])) {
-                $id_status = $_POST['id_status'];
-                $feedback = $_POST['feedback'];
-                $conn->query("UPDATE status_laporan SET feedback='$feedback', status='unapprove' where id_status='$id_status'");
-                echo "<script>alert('Laporan Berhasil Di Delete')</script>";
-                echo "<script>location='dashboard.php';</script>";
-            }
-            ?>
+    if (isset($_POST['approve'])) {
+        $id_status = $_POST['id_status'];
+        $feedback = $_POST['feedback'];
+        $conn->query("UPDATE status_laporan SET feedback='$feedback', status='approve' where id_status='$id_status'");
+        echo "<script>alert('Laporan Berhasil Di Approve')</script>";
+        echo "<script>location='dashboard.php';</script>";
+    }
+    ?>
+    <?php
+    if (isset($_POST['unapprove'])) {
+        $id_status = $_POST['id_status'];
+        $feedback = $_POST['feedback'];
+        $conn->query("UPDATE status_laporan SET feedback='$feedback', status='unapprove' where id_status='$id_status'");
+        echo "<script>alert('Laporan Berhasil Di Delete')</script>";
+        echo "<script>location='dashboard.php';</script>";
+    }
+    ?>
 
     <!-- kondisi approve -->
     <?php while ($perlaporan1 = $approve->fetch_assoc()) {; ?>
-    <div>
-        <div class="laporan">
-        <img src="../assets/fotobukti/<?= $perlaporan1['foto']; ?>" alt="bukti_laporan" width="170" height="170" />
-        <div class="detail_laporan">
-            <h4 class="pengusul">Pengusul: <span><?php echo $perlaporan1['nama']; ?></span></h3>
-                <h4 class="category">&nbsp;&nbsp;&nbsp;&nbsp;#<span><?php echo $perlaporan1['nama_kategori']; ?></span></h4>
-                <p>
-                Usulan: <br />
-                <span><?php echo $perlaporan1['keluhan'] ?>.</span>
-                </p>
-        </div>
-        <div class="icon_status approved">
-              <img src="../assets/img/unapproved.png" alt="status-icon">
-              <p>APPROVE</p>
+        <div>
+            <div class="laporan">
+                <img src="../assets/fotobukti/<?= $perlaporan1['foto']; ?>" alt="bukti_laporan" width="170" height="170" />
+                <div class="detail_laporan">
+                    <h4 class="pengusul">Pengusul: <span><?php echo $perlaporan1['nama']; ?></span></h3>
+                        <h4 class="category">&nbsp;&nbsp;&nbsp;&nbsp;#<span><?php echo $perlaporan1['nama_kategori']; ?></span></h4>
+                        <p>
+                            Usulan: <br />
+                            <span><?php echo $perlaporan1['keluhan'] ?>.</span>
+                        </p>
+                </div>
+                <div class="icon_status approved">
+                    <img src="../assets/img/unapproved.png" alt="status-icon">
+                    <p>APPROVE</p>
+                </div>
             </div>
-        </div>
-        <div class="form">
-        <textarea placeholder="tidak ada feedback" <?php if( $perlaporan1['feedback']){ echo "readonly";} ?> name="feedback" id="feedback" cols="10" rows="1" required><?php echo $perlaporan1['feedback']; ?></textarea>
-        </div>
-        </form>
+            <div class="form">
+                <textarea placeholder="tidak ada feedback" <?php if ($perlaporan1['feedback']) {
+                                                                echo "readonly";
+                                                            } ?> name="feedback" id="feedback" cols="10" rows="1" required><?php echo $perlaporan1['feedback']; ?></textarea>
+            </div>
+            </form>
         </div>
     <?php } ?>
 
     <!-- kondisi unapprove -->
     <?php while ($perlaporan2 = $unapprove->fetch_assoc()) {; ?>
-    <div>
-        <div class="laporan">
-        <img src="../assets/fotobukti/<?= $perlaporan2['foto']; ?>" alt="bukti_laporan" width="170" height="170" />
-        <div class="detail_laporan">
-            <h4 class="pengusul">Pengusul: <span><?php echo $perlaporan2['nama']; ?></span></h3>
-                <h4 class="category">&nbsp;&nbsp;&nbsp;&nbsp;#<span><?php echo $perlaporan2['nama_kategori']; ?></span></h4>
-                <p>
-                Usulan: <br />
-                <span><?php echo $perlaporan2['keluhan'] ?>.</span>
-                </p>
-        </div>
-        <div class="icon_status unapproved">
-              <img src="../assets/img/unapproved.png" alt="status-icon">
-              <p>UNAPPROVE</p>
+        <div>
+            <div class="laporan">
+                <img src="../assets/fotobukti/<?= $perlaporan2['foto']; ?>" alt="bukti_laporan" width="170" height="170" />
+                <div class="detail_laporan">
+                    <h4 class="pengusul">Pengusul: <span><?php echo $perlaporan2['nama']; ?></span></h3>
+                        <h4 class="category">&nbsp;&nbsp;&nbsp;&nbsp;#<span><?php echo $perlaporan2['nama_kategori']; ?></span></h4>
+                        <p>
+                            Usulan: <br />
+                            <span><?php echo $perlaporan2['keluhan'] ?>.</span>
+                        </p>
+                </div>
+                <div class="icon_status unapproved">
+                    <img src="../assets/img/unapproved.png" alt="status-icon">
+                    <p>UNAPPROVE</p>
+                </div>
             </div>
+            <div class="form">
+                <textarea placeholder="tidak ada feedback" name="feedback" <?php if ($perlaporan2['feedback']) {
+                                                                                echo "readonly";
+                                                                            } ?> id="feedback" cols="10" rows="1" required><?php echo $perlaporan2['feedback']; ?></textarea>
+            </div>
+            </form>
         </div>
-        <div class="form">
-        <textarea placeholder="tidak ada feedback" name="feedback" <?php if( $perlaporan2['feedback']){ echo "readonly";} ?> id="feedback" cols="10" rows="1" required><?php echo $perlaporan2['feedback']; ?></textarea>
-        </div>
-        </form>
-        </div>
-    <?php } 
+    <?php }
 } ?>
 
 <?php if ($keyword == 'all') {
@@ -108,3 +112,11 @@ if ($keyword == "Kesiswaan" or "Kurikulum" or "Sarpras" or "Humas") { ?>
 <?php if ($keyword == 'setuju' or 'tidakSetuju') {
     include 'riwayat_laporan.php';
 } ?>
+
+<?php
+if ($keyword == "Kesiswaan" or "Kurikulum" or "Sarpras" or "Humas") {
+    if (mysqli_num_rows($terkirim) == 0 && mysqli_num_rows($approve) == 0 && mysqli_num_rows($unapprove) == 0) { ?>
+        <h1 style="text-align: center; font-weight: bold; font-size: 16px; margin-top: 2px; margin-bottom: -76px">Masih kosong semua kack!<br>Harap bersabar....</h1>
+<?php  }
+}
+?>
